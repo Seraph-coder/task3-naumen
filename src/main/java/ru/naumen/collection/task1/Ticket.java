c681b70cd896485b4ae3dfb30b4670b323507743
Marchenko Dmitriy <dimamarch2014@gmail.com>
2025-10-05 23:42:07 +0500

    Сделал 1 задание

--- src/main/java/ru/naumen/collection/task1/Ticket.java
+++ src/main/java/ru/naumen/collection/task1/Ticket.java
@@ -9,4 +9,30 @@ package ru.naumen.collection.task1;
 public class Ticket {
     private long id;
     private String client;
+
+    /**
+     * Сравнивает текущий билет с другим объектом на равенство.
+     * Используется для корректной работы HashMap
+     *
+     * @param o   Объект, с которым выполняется сравнение
+     * @return    {@code true}, если оба объекта представляют один и тот же билет (по {@code id});
+     *            {@code  false} - в ином случае
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true; // Быстрая проверка для случая совпадения ссылок
+        if (o == null || getClass() != o.getClass()) return false; // Проверка типа объекта
+        Ticket ticket = (Ticket) o;
+        return id == ticket.id; // Сравнение по id
+    }
+
+    /**
+     * Возвращает хеш-код билета.
+     * Хеш-код рассчитывается на основе {@code id}, для согласования с {@link #equals(Object)}.
+     * @return хеш-код, вычисленный из id билета
+     */
+    @Override
+    public int hashCode() {
+        return Long.hashCode(id);
+    }
 }
